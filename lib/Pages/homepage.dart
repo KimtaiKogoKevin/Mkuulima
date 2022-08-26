d2e36a924366d1ce6b58c4c632a45335d9b324fd
KimtaiKogoKevin <49808720+KimtaiKogoKevin@users.noreply.github.com>
2022-08-26 09:56:31 +0300

    Updating ui

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,15 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:mkuulima/widgets/home_app_bar.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView(
+        children: [
+          const HomeAppBar(),
+          Container(
+              height: 500,
+              padding: const EdgeInsets.only(top: 15),
+              decoration: const BoxDecoration(
+                  color: Color(0xFFEDECF2),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(35),
+                    topRight: Radius.circular(35),
+                  )),
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 15),
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(left: 5),
+                          height: 50,
+                          width: 300,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Search here ..."),
+                          ),
+                        ),
+                        const Icon(Icons.camera_alt),
+
+                      ],
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                    child: Text(
+                      "Categories",
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF4C53A5)),
+                    ),
+                  ),
+                  //Categories
+                  // CategoriesWidget(),
+                ],
+              ))
+        ],
+      ),
+    );
   }
 }
