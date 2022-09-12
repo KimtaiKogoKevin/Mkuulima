3c24a57a440cdbf01cdefedd3f2e6c47f51bd545
KimtaiKogoKevin <49808720+KimtaiKogoKevin@users.noreply.github.com>
2022-09-13 00:01:26 +0300

    updates to tab bar and itempage

--- lib/Pages/ItemPage.dart
+++ lib/Pages/ItemPage.dart
@@ -9,33 +9,60 @@ class ItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: () {},
+          child: Icon(Icons.arrow_back, size: 30, color: Color(0xFF4C53A5)),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          children: [
+
+            const Padding(
+                padding: EdgeInsets.only(left: 0),
+                child: Text('Product',
+                    style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF4C53A5)))),
+            Spacer(),
+            const Icon(Icons.favorite, size: 30, color: Colors.red)
+          ],
+        ),
+      ),
       body: Column(
         children: [
-          ItemAppBar(),
+          //ItemAppBar(),
+
           Padding(
             padding:EdgeInsets.all(16),
-            child: Image.asset("images/1.png", height:400),
+            child: Image.asset("images/1.jpg", height:400),
           ),
        Arc(
          edge: Edge.TOP,
          arcType: ArcType.CONVEX,
          height:30,
-         child:Container(
-           width:double.infinity,
-           color:Colors.white,
-           child: const Padding(
-             padding: EdgeInsets.symmetric(horizontal:30),
+         child:Expanded(
+           child: Container(
+             height: 60,
+             width:double.infinity,
+             color:Color(0xFFEDECF2),
+             child: const Padding(
+               padding: EdgeInsets.symmetric(horizontal:30),
 
+             ),
            ),
          )
 
 
        ),
+          Container()
 
-          ListView(
-            children: const [
-            ],
-          ),
+          // // ListView(
+          // //   children: const [
+          // //   ],
+          // ),
         ],
       ),
       );
--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -12,112 +12,111 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      initialIndex: 1,
+      initialIndex: 0,
       length: 3,
       child: Scaffold(
         appBar: AppBar(
-          // backgroundColor: const Color(0xFF4C53A5),
-
           backgroundColor: Colors.white,
           elevation: 0,
-
-          title: const Text('MkuuLima', style: TextStyle(
-              fontWeight: FontWeight.bold, color: Color(0xFF4C53A5))),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('MkuuLima',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Color(0xFF4C53A5))),
+              InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(context, 'cartPage');
+                  },
+                  child: Icon(Icons.shopping_bag_outlined,
+                      size: 30, color: Color(0xFF4C53A5)))
+            ],
+          ),
           bottom: const TabBar(
-            indicatorColor:Color(0xFF4C53A5) ,
+            indicatorColor: Color(0xFF4C53A5),
             tabs: <Widget>[
               Tab(
-                icon:  Icon(Icons.list, size: 30, color: Color(0xFF4C53A5)),
-
-
+                icon: Icon(Icons.home, size: 30, color: Color(0xFF4C53A5)),
               ),
               Tab(
-                icon: Icon(Icons.home, size: 30 , color: Color(0xFF4C53A5)),
-
+                icon: Icon(Icons.list, size: 30, color: Color(0xFF4C53A5)),
               ),
               Tab(
-                icon:  Icon(Icons.shopping_bag_outlined, size: 30, color: Color(0xFF4C53A5)),
+                icon: Icon(Icons.person, size: 30, color: Color(0xFF4C53A5)),
               ),
             ],
           ),
         ),
-        body: TabBarView(
+        body: TabBarView(children: [
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      // Container(
+                      //     margin: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                      //     padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                      //     child: const HomeAppBar()),
+                      Container(
+                          padding: const EdgeInsets.only(top: 15),
+                          decoration: const BoxDecoration(
+                              color: Color(0xFFEDECF2),
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(35),
+                                topRight: Radius.circular(35),
+                              )),
+                          child: Column(
+                            children: [
+                              //search widget
+                              SearchBar(),
 
-          children:[
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20)
-              ),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: ListView(
-                      shrinkWrap: true,
-                      children: [
-                        // Container(
-                        //     margin: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                        //     padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-                        //     child: const HomeAppBar()),
-                        Container(
-                            padding: const EdgeInsets.only(top: 15),
-                            decoration: const BoxDecoration(
-                                color: Color(0xFFEDECF2),
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(35),
-                                  topRight: Radius.circular(35),
-                                )),
-                            child: Column(
-                              children: [
-                                //search widget
-                                SearchBar(),
-
-                                Container(
-                                  alignment: Alignment.centerLeft,
-                                  margin: EdgeInsets.symmetric(
-                                      vertical: 20, horizontal: 20),
-                                  child: const Text(
-                                    "Categories",
-                                    style: TextStyle(
-                                        fontSize: 25,
-                                        fontWeight: FontWeight.bold,
-                                        color: Color(0xFF4C53A5)),
-                                  ),
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                margin: EdgeInsets.symmetric(
+                                    vertical: 20, horizontal: 20),
+                                child: const Text(
+                                  "Categories",
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xFF4C53A5)),
                                 ),
-                                //Categories
-                                CategoriesWidget(),
+                              ),
+                              //Categories
+                              CategoriesWidget(),
 
-                                Container(
-                                  alignment: Alignment.centerLeft,
-                                  margin: EdgeInsets.symmetric(
-                                      vertical: 20, horizontal: 20),
-                                  child: const Text(
-                                    "Best Sellers",
-                                    style: TextStyle(
-                                        fontSize: 25,
-                                        fontWeight: FontWeight.bold,
-                                        color: Color(0xFF4C53A5)),
-                                  ),
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                margin: EdgeInsets.symmetric(
+                                    vertical: 20, horizontal: 20),
+                                child: const Text(
+                                  "Best Sellers",
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xFF4C53A5)),
                                 ),
-                                ItemsWidget()
-                              ],
-                            ))
-                      ],
-                    ),
+                              ),
+                              ItemsWidget()
+                            ],
+                          ))
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            const Center(
-              child: Text("It's cloudy here"),
-            ),
-            Center(
-              child: Text("It's rainy here"),
-            ),
-
-
-          ]
-        ),
+          ),
+          const Center(
+            child: Text("It's cloudy here"),
+          ),
+          Center(
+            child: Text("It's rainy here"),
+          ),
+        ]),
         // bottomNavigationBar: CurvedNavigationBar(
         //   items: <Widget>[
         //     Icon(Icons.home, size: 30),
